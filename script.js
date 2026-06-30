17f262c0823a92b05eaa4271f5783a7f798c487c
Hope11710 <amariibarron@gmail.com>
2026-06-30 08:35:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function showPopup(text) {
+    const popup = document.getElementById("popup");
+    const content = document.getElementById("popupContent");
+
+    popup.innerHTML = text;
+    popup.classList.remove("hidden");
+}
+
+function showInfo(type) {
+
+    if (type == "help") {
+        showPopup(`
+            <b>Things I Help With</b><br><br>
+            Give medicine<br>
+            Check how you're feeling<br>
+            Help you feel safe
+            <br><br>
+            <small>Tap anywhere to close</small>
+        `);
+    }
+
+    else if (type === "tools") {
+        showPopup(`
+            <b>My Tools</b><br><br>
+            Stethoscope<br>
+            Thermometer<br>
+            Blood pressure cuff
+        `);
+    }
+
+    else if (type === "fun") {
+        showPopup(`
+            <b>Fun Fact</b><br><br>
+            Nurses are one of the most important parts of a hospital team!
+        `);
+    }
+
+    function closePopup(event) {
+        const popup = document.getElementById("popup");
+        popup.classList.add("hidden");
+    }
+}
